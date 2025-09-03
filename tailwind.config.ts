512a7dcea84e42a21de95ba53020df55760a3dba
Cetc2211 <mpceciliotopetecruz@gmail.com>
2025-09-03 00:08:52 -0700

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,122 @@
+import type {Config} from 'tailwindcss';
+
+export default {
+  darkMode: ['class'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['"PT Sans"', 'sans-serif'],
+        headline: ['"PT Sans"', 'sans-serif'],
+        code: ['monospace'],
+      },
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+          'foreground-alt': 'hsl(var(--card-foreground-alt))',
+          '1': 'hsl(var(--card-1))',
+          '2': 'hsl(var(--card-2))',
+          '3': 'hsl(var(--card-3))',
+          '4': 'hsl(var(--card-4))',
+          '5': 'hsl(var(--card-5))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
+        sidebar: {
+          DEFAULT: 'hsl(var(--sidebar-background))',
+          foreground: 'hsl(var(--sidebar-foreground))',
+          primary: 'hsl(var(--sidebar-primary))',
+          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+          accent: 'hsl(var(--sidebar-accent))',
+          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+          border: 'hsl(var(--sidebar-border))',
+          ring: 'hsl(var(--sidebar-ring))',
+        },
+        partial: {
+          '1': 'hsl(var(--partial-1))',
+          '1-foreground': 'hsl(var(--partial-1-foreground))',
+          '1-foreground-alt': 'hsl(var(--partial-1-foreground-alt))',
+          '1-border': 'hsl(var(--partial-1-border))',
+          '1-bg': 'hsl(var(--partial-1-bg))',
+          '2': 'hsl(var(--partial-2))',
+          '2-foreground': 'hsl(var(--partial-2-foreground))',
+          '2-foreground-alt': 'hsl(var(--partial-2-foreground-alt))',
+          '2-border': 'hsl(var(--partial-2-border))',
+          '2-bg': 'hsl(var(--partial-2-bg))',
+          '3': 'hsl(var(--partial-3))',
+          '3-foreground': 'hsl(var(--partial-3-foreground))',
+          '3-foreground-alt': 'hsl(var(--partial-3-foreground-alt))',
+          '3-border': 'hsl(var(--partial-3-border))',
+          '3-bg': 'hsl(var(--partial-3-bg))',
+        }
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: {
+            height: '0',
+          },
+          to: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+        },
+        'accordion-up': {
+          from: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+          to: {
+            height: '0',
+          },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
