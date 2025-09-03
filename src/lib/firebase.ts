6e8cfe45ab6ebdc80612820ae2d53d7bdaeb1673
Cetc2211 <mpceciliotopetecruz@gmail.com>
2025-09-03 10:08:47 -0700

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+// src/lib/firebase.ts
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  // Configuración de Firebase desactivada temporalmente para despliegue en Netlify
+  // "projectId": "actracker-master",
+  // "appId": "1:660718374201:web:4889a6d15d8aee23ddace8",
+  // "storageBucket": "actracker-master.firebasestorage.app",
+  // "apiKey": "AIzaSyCDy-W8_3sB3WS8gVKZuzV_P6PdG1tBOUc",
+  // "authDomain": "actracker-master.firebaseapp.com",
+  // "messagingSenderId": "660718374201",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
