d0ff888a7612048e8d919758c876b02a26ff5fb1
Cetc2211 <mpceciliotopetecruz@gmail.com>
2025-09-04 21:30:08 -0700

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // Configuración de Firebase desactivada temporalmente para despliegue en Netlify
-  // "projectId": "actracker-master",
-  // "appId": "1:660718374201:web:4889a6d15d8aee23ddace8",
-  // "storageBucket": "actracker-master.firebasestorage.app",
-  // "apiKey": "AIzaSyCDy-W8_3sB3WS8gVKZuzV_P6PdG1tBOUc",
-  // "authDomain": "actracker-master.firebaseapp.com",
-  // "messagingSenderId": "660718374201",
+    "projectId": "actracker-master",
+    "appId": "1:660718374201:web:4889a6d15d8aee23ddace8",
+    "storageBucket": "actracker-master.firebasestorage.app",
+    "apiKey": "AIzaSyCDy-W8_3sB3WS8gVKZuzV_P6PdG1tBOUc",
+    "authDomain": "actracker-master.firebaseapp.com",
+    "messagingSenderId": "660718374201"
 };
 
 // Initialize Firebase
